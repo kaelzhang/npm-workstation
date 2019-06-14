f94b0d636d70bd706dcf9f07a645289d5ce5e8e1
Kael Zhang <i+github@kael.me>
2019-06-14 22:27:10 +0800

    link: fixes chmod

--- src/link.js
+++ src/link.js
@@ -1,9 +1,15 @@
 const path = require('path')
 const fs = require('fs-extra')
+const {chmod} = require('fs-chmod')
 
 const createLink = async (target, source) => {
   await fs.remove(target)
   await fs.symlink(source, target)
+}
+
+const createBinLink = async (target, source) => {
+  await createLink(target, source)
+  await chmod(target, 'ug+x')
 }
 
 const link = async pc => {
@@ -28,7 +34,7 @@ const link = async pc => {
       for (const [name, binPath] of Object.entries(bin)) {
         const binTarget = path.join(
           dependent.path, 'node_modules', '.bin', name)
-        tasks.push(createLink(binTarget, binPath))
+        tasks.push(createBinLink(binTarget, binPath))
       }
     }
   }
